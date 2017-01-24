967f5d3053c757f27d3d84355a7668ef9b942079
Shameel Abdul <abduls@upmc.edu>
2017-01-24 17:04:21 -0500

    Project Created

---
+++ src/main/java/com/findtutor/FindtutorApplication.java
@@ -0,0 +1,12 @@
+package com.findtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindtutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindtutorApplication.class, args);
+	}
+}
