8e36d452f5a5b0b6ca2a4468b9fa51c00f8789d1
Shameel Abdul <abduls@upmc.edu>
2017-01-24 16:41:16 -0500

    Hellow world

---
+++ src/main/java/com/findtutor/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.findtutor.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String sayHello()
+	{
+		return "index";
+	}
+}
