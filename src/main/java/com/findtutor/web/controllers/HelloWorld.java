335d6d6d2794e10d1da4be639f7e9600430e74ed
Shameel Abdul <abduls@upmc.edu>
2017-01-25 00:00:05 -0500

    Parent level Packages created

--- src/main/java/com/findtutor/web/controllers/HelloWorld.java
+++ src/main/java/com/findtutor/web/controllers/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.findtutor.controllers;
+package com.findtutor.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
